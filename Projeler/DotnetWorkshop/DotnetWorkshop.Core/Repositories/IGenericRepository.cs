d7b7df6f2f18167db76e8ecc7040186e4ed5be74
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2024-02-09 19:54:28 +0300

    IGenericRepository geliştirmeleri

--- Projeler/DotnetWorkshop/DotnetWorkshop.Core/Repositories/IGenericRepository.cs
+++ Projeler/DotnetWorkshop/DotnetWorkshop.Core/Repositories/IGenericRepository.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotnetWorkshop.Core.Repositories
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id);
+
+        //user.GetAll(s => s.userName == "kardel").ToList();
+        IQueryable<T> GetAll();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
