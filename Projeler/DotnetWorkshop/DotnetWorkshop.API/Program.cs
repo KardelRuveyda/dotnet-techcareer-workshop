e663c825f140701312d9377362bc7921e7d64407
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2024-02-10 20:54:58 +0300

    Validasyon işlemleri

--- Projeler/DotnetWorkshop/DotnetWorkshop.API/Program.cs
+++ Projeler/DotnetWorkshop/DotnetWorkshop.API/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using DotnetWorkshop.Core.Repositories;
 using DotnetWorkshop.Core.Services;
 using DotnetWorkshop.Core.UnitOfWorks;
@@ -6,7 +7,10 @@ using DotnetWorkshop.Repository.Repositories;
 using DotnetWorkshop.Repository.UnitOfWorks;
 using DotnetWorkshop.Service.Mapping;
 using DotnetWorkshop.Service.Services;
+using DotnetWorkshop.Service.Validations;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,19 +22,73 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+#region Swagger Ýþlemleri
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
+#endregion
+
+
+//Automapper kütüphanesinin tanýmlanmasý
 builder.Services.AddAutoMapper(typeof(MapProfile));
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
-builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
+builder.Services.AddHttpContextAccessor();
+
+
+//Fluent Validation Tanýmlamasýnýn gerçekleþtirilmesi.
+//1. Yöntem
+//builder.Services.AddControllers()
+//    .AddFluentValidation(x =>
+//    {
+//        x.RegisterValidatorsFromAssemblyContaining<TeamDtoValidator>();
+//        x.RegisterValidatorsFromAssemblyContaining<UserDtoValidator>();
+//        x.RegisterValidatorsFromAssemblyContaining<UserProfileDtoValidator>();
+//    });
+
+//2. yöntem
+builder.Services.AddControllers().AddFluentValidation(x =>
+{
+    x.RegisterValidatorsFromAssemblyContaining<TeamDtoValidator>();
+});
+
 
 //AppDbContext ile ilgili iþlemler
-
 builder.Services.AddDbContext<AppDbContext>(x =>
   x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
   {
       option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
   })
-); ;
+);
+
+
+//Autofac kütüphanesini ekledik. Bu kütüphane aracýlýðýyla (Generic Repository,service vb) iþlemleri otomatik olarak program.cs'de çalýþtýrsýn.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+// Buranýn devamý (API KATMANINDA.)
 
 var app = builder.Build();
 
