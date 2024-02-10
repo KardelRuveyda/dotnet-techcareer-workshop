a3e8ac2b2cad92c8490a03eaf5099e22bdae3447
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2024-02-10 20:13:48 +0300

    Services işlemleri eklendi.TeamService,UserService,UserProfileService

---
+++ Projeler/DotnetWorkshop/DotnetWorkshop.Service/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using DotnetWorkshop.Core.Models;
+using DotnetWorkshop.Core.Repositories;
+using DotnetWorkshop.Core.Services;
+using DotnetWorkshop.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetWorkshop.Service.Services
+{
+    public class UserService : Service<User>, IUserService
+    {
+        public UserService(IGenericRepository<User> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
